3fe4399f826aaefd49a6bd58c06157b19a6baaa8
vasudev290 <vasukesavulu@gmail.com>
2024-03-15 17:44:27 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,21 @@
+var a=10
+var b=20
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+
+var Factor="Arjun"
+var Fplayer="M.S Dhoni"
+var Fmovie="Ala vaikunthapuram"
+
+function Favourite()
+{
+    console.log("Favourite actor:"+"arjun")
+    console.log("Favourite player:"+"M.S. Dhoni")
+    console.log("Favourite movie:"+"Ala vaikunthapuram")
+     
+}
+Favourite()
